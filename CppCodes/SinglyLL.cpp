edea5b31ff875775db149b3c389bc1de361ddc49
DheAtom07 <hattnikalbhag@gmail.com>
2025-02-04 18:44:08 +0530

    Some more feature

--- CppCodes/SinglyLL.cpp
+++ CppCodes/SinglyLL.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 struct Node{
     int value;
     Node* next;
@@ -9,14 +8,14 @@ struct Node{
         next=NULL;
     }
 };
-
-void insertAtStarting(Node* &Head,int v){
+void insertAtStarting(Node* &Head,int v)
+{
     Node* newElement=new Node(v);
     newElement->next=Head;
     Head=newElement;
 }
-
-void insertAsIthNode(Node* Head,int i, int j){
+void insertAsIthElement(Node* Head,int i, int j)
+{
     Node* newElement=new Node(j);
     int k=2;
     Node* temp=Head;
@@ -27,17 +26,42 @@ void insertAsIthNode(Node* Head,int i, int j){
     newElement->next=temp->next;
     temp->next=newElement;
 }
-
-void display(Node* Head){
+void deleteIthElement(Node* Head,int i)
+{
+    Node* temp=Head;
+    for( int k=1 ; k < i-1 ; k++){
+        temp=temp->next;
+    }
+    temp->next=temp->next->next;
+}
+void display(Node* Head)
+{
     Node* temp=Head;
     while (temp!=NULL){
         cout<<" -> "<<temp->value;
         temp=temp->next;
     }
 }
-
+void deleteElement(Node* Head,int i,int j)
+{                     // Deletes ith repetation of j
+    int counter1;
+    // cout<<"Hello";
+    counter1=0;
+    Node* temp=Head;
+    while (counter1<i){
+        temp=temp->next;
+        if (temp->value==j){
+            counter1++;
+        }
+        cout<<"Hello";
+        if (temp==NULL) {
+            cout<<"Hag diyaa ...."<<endl;
+            break;
+        }
+    }
+    temp->next=temp->next->next;
+}
 int main(){
-
     Node* Head=NULL;
     int j;
     for(int i=0;i<10;i++){
@@ -45,7 +69,9 @@ int main(){
         cin>>j;
         insertAtStarting(Head,j);
     }
-    insertAsIthNode(Head,4,11);
+    insertAsIthElement(Head,4,11);
+    // deleteIthElement(Head,7);
+    deleteElement(Head, 3, 4);
     cout<<"Kindly if you want to enter starting element use That function. "<<endl;
     display(Head);
 }
